20c5da650d94cb246943563aca6fb4356124cf59
SoundhariyaSi <128037023+SoundhariyaSi@users.noreply.github.com>
2023-03-29 12:47:05 +0530

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Components/Styles.css'
+
+function Header() {
+  return (
+    <div className="head">
+      <h1 className="title">Blue Farms</h1>
+      <p className="para">Our cozy little cafe is the perfect place to spend an evening with your friends and loved ones. Unique and one of a kind teas,coffees and pastries can be found here. </p>
+      
+    </div>
+  )
+}
+
+export default Header
